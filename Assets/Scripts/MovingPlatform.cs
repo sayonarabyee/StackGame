686e9b60ceaaa3061c5b2d84f15d58a274f02a0d
SashaV <sasha1404568@gmail.com>
2020-07-06 21:45:51 +0300

    Initial commit

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float platformSpeed = 5f;
+    public Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        
+    }
+
+    void Update()
+    {
+        transform.position += new Vector3(0, 0, platformSpeed * Time.deltaTime);
+        platformStop();
+       
+    }
+    void platformStop()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            platformSpeed = 0;
+            //BreakPlatform()
+        }
+    }
+
+    void BreakPlatform()
+    {
+        //Double mechanic
+    }
+}
