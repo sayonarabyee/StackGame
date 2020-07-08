51d7dc748b43782ac297d1ecc8a2a5dc66c4ae0d
yokoosama <38509676+yokoosama@users.noreply.github.com>
2020-07-08 23:58:20 +0300

    moved files to src/StackGame.Unity

---
+++ src/StackGame.Unity/Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public float speed;
+    
+    void Update()
+    {
+        var speed = this.speed * Time.deltaTime;
+        transform.position += new Vector3(0, 0, speed);
+    }
+}
