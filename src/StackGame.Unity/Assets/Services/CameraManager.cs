13c3ee9ee879932839c71743a819cefdaa57e470
Dmitriy <dmitriy.vorobey1@gmail.com>
2020-07-22 23:34:29 +0300

    added camera movement (#31)
    * added camera movement
    
    * added part of previous commit
    
    * fixed tests
    
    Co-authored-by: kakashi_10@mail.ru <Mp2963145>

---
+++ src/StackGame.Unity/Assets/Services/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using Interfaces;
+using UnityEngine;
+
+namespace Services
+{
+    public class CameraManager : ICameraManager
+    {
+        private readonly Camera _camera;
+
+        public CameraManager(Camera camera)
+        {
+            _camera = camera;
+        }
+
+        public void MoveUp(float distance)
+        {
+            var cameraPosition = _camera.gameObject.transform.position;
+            cameraPosition.y += distance;
+            _camera.gameObject.transform.position = cameraPosition;
+        }
+    }
+}
